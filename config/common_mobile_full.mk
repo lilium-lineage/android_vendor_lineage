e37ec57bf6f8d3ec7c11a0f658ee8d68c7cd79c4
HeroBuxx <me@herobuxx.me>
2025-06-05 20:53:44 +0800

    lineage: config: common: Include HalcyonUI
    Change-Id: I1ba2c9c89ed16544795a114635397c7f82ae8de7
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -31,12 +31,6 @@ PRODUCT_PACKAGES += \
     unrar \
     zstd
 
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontLatoOverlay \
-    FontRubikOverlay
-
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
